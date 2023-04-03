4a7b3cd3391c3e782994d823a892521a6b63ae49
DDM <60308730+dmdobrea@users.noreply.github.com>
2023-04-04 00:56:48 +0300

    Add files via upload

--- agri_hovergames/launch/python_parameters_launch.py
+++ agri_hovergames/launch/python_parameters_launch.py
@@ -10,10 +10,57 @@ def generate_launch_description():
             output='screen',
             emulate_tty=True,
             parameters=[
-                {'verbose':  1},
-                {'NavQplus': 1},
-                {'width':    640},
-                {'height':   480}
+                {'verbose':   0},
+                {'NavQplus':  1},
+                {'file_path': 'None'},        # path to the video, like '/home/user/FilmTest.mp4' or 'None'
+                {'width':     640},
+                {'height':    480}
+            ]
+        ),
+        
+        Node(
+            package='agri_hovergames',
+            executable='flightHGd',
+            name='custom_flightHGd',
+            output='screen',
+            emulate_tty=True,
+            parameters=[
+                {'verbose':    0},
+                {'depth_cam':  1}
+            ]
+        ),        
+        
+        Node(
+            package='agri_hovergames',
+            executable='healthHGd',
+            name='custom_healthHGd',
+            output='screen',
+            emulate_tty=True,
+            parameters=[
+                {'verbose':     1},
+                {'NavQplus':    1},
+                {'file_path':  '/home/user/FilmTest.mp4'},        # path to the video, like '/home/user/FilmTest.mp4' or 'None'
+                {'width':       640},
+                {'height':      480},
+                {'target':     'npu'},                              # target = {'cpu', 'gpu', 'npu'}
+                {'no_threads':  1},
+                {'model_path': '/home/user/detection-precision-npu-in-uint8_out-uint8_channel_ptq.tflite'},
+                {'mean':        127.5},
+                {'stdv':        127.5},
+                {'prob_thr':    0.9}
+            ]
+        ),        
+        
+        Node(
+            package='agri_hovergames',
+            executable='broadcastHGd',
+            name='custom_broadcastHGd',
+            output='screen',
+            emulate_tty=True,
+            parameters=[
+                {'verbose':     0},
+                {'server_ip':  '192.168.0.101'},
+                {'source_sel':  2}                 # 0 - from videp_publisher_node (videoHGd), 1 - from flight_control_node (flightHGd), 2 - from health_plant_node
             ]
         )
     ])
